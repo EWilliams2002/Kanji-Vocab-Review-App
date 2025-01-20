ed59b1260d313c6a0b11be6fddfa645fc33874be
Evan Williams <157436047+EWilliams2002@users.noreply.github.com>
2025-01-20 06:32:03 -0800

    - implemented vocab portion
    -program almost complete besides adding new kanji

--- start.py
+++ start.py
@@ -73,7 +73,7 @@ def get_dlinked_list(choice_dict):
 
 
 
-def change_card(curr, direction, extras, win, rect1, rect12):
+def change_card(curr, direction, extras, win, rect1, rect12, choice):
     """
     Add two numbers.
 
@@ -81,58 +81,144 @@ def change_card(curr, direction, extras, win, rect1, rect12):
     :param y: Second number
     :return: Sum of x and y
     """
+    if choice == 'kanji':
 
-    # kanji - extras = [char, kun, on, kun_sentence, on_sentence]
-    char = extras[0]
-    kun = extras[1]
-    on = extras[2]
-    kun_sentence = extras[3]
-    on_sentence = extras[4]
+        # kanji - extras = [char, kun, on, kun_sentence, on_sentence]
+        char = extras[0]
+        kun = extras[1]
+        on = extras[2]
+        kun_sentence = extras[3]
+        on_sentence = extras[4]
 
-    if direction == 'r':
-        kanji = curr.next.data
+        if direction == 'r':
+            kanji = curr.next.data
 
-    if direction == 'l':
-        kanji = curr.prev.data
+        if direction == 'l':
+            kanji = curr.prev.data
 
-    char.undraw()
-    kun.undraw()
-    on.undraw()
-    kun_sentence.undraw()
-    on_sentence.undraw()
+        char.undraw()
+        kun.undraw()
+        on.undraw()
+        kun_sentence.undraw()
+        on_sentence.undraw()
 
-    # The kanji char
-    char = Text(Point(1000, 300), kanji.char)
-    char.setSize(36)
-    char.draw(win)
+        # The kanji char
+        char = Text(Point(1000, 300), kanji.char)
+        char.setSize(36)
+        char.draw(win)
 
-    # kun yomi
-    kun = Text(Point(725, 530),kanji.kun_yomi)
-    kun.setSize(15)
-    kun.draw(win)
+        # kun yomi
+        kun = Text(Point(725, 530),kanji.kun_yomi)
+        kun.setSize(15)
+        kun.draw(win)
 
-    # on yomi
-    on = Text(Point(1275, 530),kanji.on_yomi)
-    on.setSize(15)
-    on.draw(win)
+        # on yomi
+        on = Text(Point(1275, 530),kanji.on_yomi)
+        on.setSize(15)
+        on.draw(win)
 
-    # kun sentence
-    kun_sentence = Text(Point(725, 630),kanji.kun_sentence)
-    kun_sentence.setSize(15)
-    kun_sentence.draw(win)
+        # kun sentence
+        kun_sentence = Text(Point(725, 630),kanji.kun_sentence)
+        kun_sentence.setSize(15)
+        kun_sentence.draw(win)
 
-    # on sentence
-    on_sentence = Text(Point(1275, 630),kanji.on_sentence)
-    on_sentence.setSize(15)
-    on_sentence.draw(win)
+        # on sentence
+        on_sentence = Text(Point(1275, 630),kanji.on_sentence)
+        on_sentence.setSize(15)
+        on_sentence.draw(win)
 
-    rect1_, rect12_ = draw_blinders(win, rect1, rect12)
+        rect1_, rect12_ = draw_blinders(win, rect1, rect12, choice)
 
-    if direction == 'r':
-        return curr.next, [char, kun, on, kun_sentence, on_sentence], rect1_, rect12_
+        if direction == 'r':
+            return curr.next, [char, kun, on, kun_sentence, on_sentence], rect1_, rect12_
 
-    if direction == 'l':
-        return curr.prev, [char, kun, on, kun_sentence, on_sentence], rect1_, rect12_
+        if direction == 'l':
+            return curr.prev, [char, kun, on, kun_sentence, on_sentence], rect1_, rect12_
+        
+    if choice == 'vocab':
+
+        word = extras[0]
+        translation = extras[1]
+        sentence_jpn = extras[2]
+        sentence_eng = extras[3]
+
+        if direction == 'r':
+            card = curr.next.data
+
+        if direction == 'l':
+            card = curr.prev.data
+
+        word.undraw()
+        translation.undraw()
+        sentence_jpn.undraw()
+        sentence_eng.undraw()
+        
+
+        # The kanji char
+        word = Text(Point(1000, 250), card.word)
+        word.setSize(36)
+        word.draw(win)
+
+        # translation
+        translation = Text(Point(1000, 365),card.translation)
+        translation.setSize(20)
+        translation.draw(win)
+
+        # divider line
+        divider = Line(Point(1000, 450), Point(1000, 650))
+        divider.draw(win)
+
+        # sentence_jpn
+        sentence_jpn = Text(Point(800, 475),card.sentence_jpn)
+        sentence_jpn.setSize(20)
+        sentence_jpn.draw(win)
+
+        # sentence_eng
+        sentence_eng = Text(Point(1200, 475),card.sentence_eng)
+        sentence_eng.setSize(20)
+        sentence_eng.draw(win)
+        
+        try:
+            present_aff = extras[4]
+            present_neg = extras[5]
+
+            present_aff.undraw()
+            present_neg.undraw()
+
+            # verb label
+            verb_label = Text(Point(800, 575),'Verb Tenses')
+            verb_label.setSize(20)
+            verb_label.draw(win)
+
+            # present_aff
+            present_aff = Text(Point(1200, 575),card.present_aff)
+            present_aff.setSize(20)
+            present_aff.draw(win)
+
+            # present_neg
+            present_neg = Text(Point(1200, 625),card.present_neg)
+            present_neg.setSize(20)
+            present_neg.draw(win)
+            
+            rect1_, rect12_ = draw_blinders(win, rect1, rect12, choice)
+            
+            if direction == 'r':
+                return curr.next, [word, translation, sentence_jpn, sentence_eng, present_aff, present_neg], rect1_, rect12_
+
+            if direction == 'l':
+                return curr.prev, [word, translation, sentence_jpn, sentence_eng, present_aff, present_neg], rect1_, rect12_
+            
+        except IndexError:
+
+            rect1_, rect12_ = draw_blinders(win, rect1, rect12, choice)
+
+            if direction == 'r':
+                return curr.next, [word, translation, sentence_jpn, sentence_eng], rect1_, rect12_
+
+            if direction == 'l':
+                return curr.prev, [word, translation, sentence_jpn, sentence_eng], rect1_, rect12_
+
+    
     
 def draw_card(win, curr, choice):
     """
@@ -222,7 +308,7 @@ def draw_card(win, curr, choice):
         if card.present_aff is not None:
 
             # verb label
-            verb_label = Text(Point(800, 575),'Verbs')
+            verb_label = Text(Point(800, 575), 'Verb Tenses')
             verb_label.setSize(20)
             verb_label.draw(win)
 
@@ -261,7 +347,7 @@ def draw_card(win, curr, choice):
         return [word, translation, sentence_jpn, sentence_eng], rect1_, rect12_
 
 
-def draw_blinders(win, rect1, rect12):
+def draw_blinders(win, rect1, rect12, choice):
     """
     Add two numbers.
 
@@ -272,18 +358,32 @@ def draw_blinders(win, rect1, rect12):
     
     undraw_blinders('l', rect1, rect12)
     undraw_blinders('r', rect1, rect12)
+    
+    if choice == 'kanji':
+        rect1_ = Rectangle(Point(575,410), Point(910,810))
+        rect1_.setOutline(color_rgb(0, 0, 0))
+        rect1_.setFill(color_rgb(150, 200, 150))
+        rect1_.draw(win)
 
-    rect1_ = Rectangle(Point(575,410), Point(910,810))
-    rect1_.setOutline(color_rgb(0, 0, 0))
-    rect1_.setFill(color_rgb(150, 200, 150))
-    rect1_.draw(win)
+        rect12_ = Rectangle(Point(1075,410), Point(1410,810))
+        rect12_.setOutline(color_rgb(0, 0, 0))
+        rect12_.setFill(color_rgb(150, 200, 150))
+        rect12_.draw(win)
 
-    rect12_ = Rectangle(Point(1075,410), Point(1410,810))
-    rect12_.setOutline(color_rgb(0, 0, 0))
-    rect12_.setFill(color_rgb(150, 200, 150))
-    rect12_.draw(win)
+        return rect1_, rect12_
+    
+    if choice == 'vocab':
+        rect1_ = Rectangle(Point(600,340), Point(1400,380))
+        rect1_.setOutline(color_rgb(0, 0, 0))
+        rect1_.setFill(color_rgb(150, 200, 150))
+        rect1_.draw(win)
 
-    return rect1_, rect12_
+        rect12_ = Rectangle(Point(600,440), Point(1400,670))
+        rect12_.setOutline(color_rgb(0, 0, 0))
+        rect12_.setFill(color_rgb(150, 200, 150))
+        rect12_.draw(win)
+
+        return rect1_, rect12_
 
 def undraw_blinders(direction, rect1, rect12):
     """
@@ -384,49 +484,65 @@ def program_loop(choice):
     if choice == 'vocab':
         extras, rect1, rect12 = draw_card(win, curr, 'vocab')
     
-    ### This is where I left off
-    ###
-    ###
-    ###
-    ###
 
-    # # program loop
-    # while not closed:
+    # program loop
+    while not closed:
     
-    #     mouse = win.getMouse()  
+        mouse = win.getMouse()  
 
-    #     # Pressed left
-    #     if (mouse.getX() >= 0 and mouse.getX() <= 449) and (mouse.getY() >= 100 and mouse.getY() <= 900) and curr.prev is not None:
+        # Pressed left
+        if (mouse.getX() >= 0 and mouse.getX() <= 449) and (mouse.getY() >= 100 and mouse.getY() <= 900) and curr.prev is not None:
 
-    #         curr, extras, rect1, rect12 = change_card(curr, 'l', extras, win, rect1, rect12)
+            curr, extras, rect1, rect12 = change_card(curr, 'l', extras, win, rect1, rect12, choice)
 
-    #         print('clicked left', mouse.getX(), mouse.getY())
+            # print('clicked left', mouse.getX(), mouse.getY())
                 
-    #     # Pressed right
-    #     if (mouse.getX() >= 1551 and mouse.getX() <= 2000) and (mouse.getY() >= 100 and mouse.getY() <= 900) and curr.next is not None:
+        # Pressed right
+        if (mouse.getX() >= 1551 and mouse.getX() <= 2000) and (mouse.getY() >= 100 and mouse.getY() <= 900) and curr.next is not None:
 
-    #         curr, extras, rect1, rect12 = change_card(curr, 'r', extras, win, rect1, rect12)
+            curr, extras, rect1, rect12 = change_card(curr, 'r', extras, win, rect1, rect12, choice)
 
-    #         print('clicked right', mouse.getX(), mouse.getY())
+            # print('clicked right', mouse.getX(), mouse.getY())
 
-    #     # Pressed exit
-    #     if (mouse.getX() >= 1900 and mouse.getX() <= 2000) and (mouse.getY() >= 950 and mouse.getY() <= 1000):
+        # Pressed exit
+        if (mouse.getX() >= 1900 and mouse.getX() <= 2000) and (mouse.getY() >= 950 and mouse.getY() <= 1000):
 
-    #         print('clicked exit', mouse.getX(), mouse.getY())
-    #         win.close()
-    #         closed = True
+            win.close()
+            closed = True
+
+            # print('clicked exit', mouse.getX(), mouse.getY())
                 
-    #     # Pressed card left
-    #     if (mouse.getX() >= 450 and mouse.getX() <= 1000) and (mouse.getY() >= 100 and mouse.getY() <= 900):
 
-    #         undraw_blinders('l', rect1, rect12)
-    #         print('clicked card left', mouse.getX(), mouse.getY())
 
-    #     # Pressed card right
-    #     if (mouse.getX() >= 1001 and mouse.getX() <= 1550) and (mouse.getY() >= 100 and mouse.getY() <= 900):
+        # Pressed card left (kanji)
+        if (mouse.getX() >= 450 and mouse.getX() <= 1000) and (mouse.getY() >= 100 and mouse.getY() <= 900) and choice == 'kanji':
 
-    #         undraw_blinders('r', rect1, rect12)
-    #         print('clicked card right', mouse.getX(), mouse.getY())
+            undraw_blinders('l', rect1, rect12)
+
+            # print('clicked card left', mouse.getX(), mouse.getY())
+
+        # Pressed card right (kanji)
+        if (mouse.getX() >= 1001 and mouse.getX() <= 1550) and (mouse.getY() >= 100 and mouse.getY() <= 900) and choice == 'kanji':
+
+            undraw_blinders('r', rect1, rect12)
+
+            # print('clicked card right', mouse.getX(), mouse.getY())
+        
+
+
+        # Pressed card upper (vocab)
+        if (mouse.getX() >= 450 and mouse.getX() <= 1550) and (mouse.getY() >= 100 and mouse.getY() <= 400) and choice == 'vocab':
+
+            undraw_blinders('l', rect1, rect12)
+
+            # print('clicked card left', mouse.getX(), mouse.getY())
+
+        # Pressed card lower (vocab)
+        if (mouse.getX() >= 450 and mouse.getX() <= 1550) and (mouse.getY() >= 401 and mouse.getY() <= 900) and choice == 'vocab':
+
+            undraw_blinders('r', rect1, rect12)
+
+            # print('clicked card right', mouse.getX(), mouse.getY())
 
 def read_to_dictionary(filename, choice):
     
@@ -490,24 +606,32 @@ def main():
         add_kanji()
 
     print("Would you like to play? [Y/N]")
-    res = input()
-    print('\n\n')
+    loop_res = input()
 
-    if res.lower() in ['y','yes']:
+    if loop_res.lower() in ['y','yes']:
 
-        print("Kanji or Vocab review? [K/V]")
-        res = input()
+        while loop_res.lower() not in ['e','exit']:
+
+            if loop_res.lower() in ['y','yes']:
+                print('\n\n')
+                
+            print("Kanji review, Vocab review, or exit? [K/V/E]")
+            loop_res = input()
+            print('\n\n')
+
+            if loop_res.lower() in ['k','kanji']:
+
+                # Starts the program loop and the window. Upon exit, the window closes and you are brought
+                # back to the goodbye prompt below
+                program_loop('kanji')
+
+            if loop_res.lower() in ['v','vocab']:
+                program_loop('vocab')
+            
+            if loop_res.lower() in ['e','exit']:
+                break
+    else:
         print('\n\n')
-
-        if res.lower() in ['k','kanji']:
-
-            # Starts the program loop and the window. Upon exit, the window closes and you are brought
-            # back to the goodbye prompt below
-            program_loop('kanji')
-
-        if res.lower() in ['v','vocab']:
-            program_loop('vocab')
-
 
     input("Goodbye !!\n\nPress enter to exit . . .")
 
